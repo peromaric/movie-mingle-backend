6482484fa5f95cbec08ba0f92113174437f9d8ad
Pero Maric <peromaric91@gmail.com>
2023-11-06 20:52:41 +0100

    Initial commit

---
+++ src/main/java/com/challenge/movieminglebackend/MovieMingleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.movieminglebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMingleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieMingleBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/challenge/movieminglebackend/MovieMingleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.movieminglebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMingleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
